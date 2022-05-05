89509b70683d5f35c88e15db8329f21972b60acf
Arhontes <arhontes@yandex.ru>
2022-05-05 11:12:25 +0300

    Installed the latest version of react-router-dom. Wrapped all in App.tsx with a tag BrowserRouter.Used Routes and Route.

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import Header from './Components/Header/Header';
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
-            <Header />
-            <NavBar />
-            {/*<Profile />*/}
-            <div className={'app-wrapper-content'}>
-                <Dialogs />
-            </div>
 
-        </div>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <NavBar/>
+
+                <div className={'app-wrapper-content'}>
+                    <Routes>
+                        <Route path="/dialogs*" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/news" element={<Profile/>}/>
+                        <Route path="/music" element={<Profile/>}/>
+                    </Routes>
+                </div>
+
+            </div>
+        </BrowserRouter>
+
     );
 }
 
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import n from './NavBar.module.css'
+
+
 const NavBar = () => {
     return (
         <nav className={n.navBar}>
-          <div className={`${n.item} ${n.active}`}><a href="">Profile</a></div>
-          <div><a href="">Dialogs</a></div>
+          <div className={`${n.item} ${n.active}`}><a href="/profile">Profile</a></div>
+          <div><a href="/dialogs">Dialogs</a></div>
           <div><a href="">News</a></div>
           <div><a href="">Music</a></div>
 
