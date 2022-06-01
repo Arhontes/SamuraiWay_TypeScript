a48d21a59ed5727129de1f868a7339094e1b1812
Alex Betke <arhontes@yandex.ru>
2022-06-01 09:57:33 +0300

    add bind for store methods

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App({dialogsPage,profilePage}:StateType) {
                 <div className={'app-wrapper-content'}>
                     <Routes>
                         <Route path="/dialogs/*" element={<Dialogs dialogsPage={dialogsPage}/>}/>
-                        <Route path="/profile" element={<Profile profilePage={profilePage} addPost={store.addPost} changeNewPostText={store.updateNewPostText}/>}/>
+                        <Route path="/profile" element={<Profile profilePage={profilePage} addPost={store.addPost.bind(store)} changeNewPostText={store.updateNewPostText.bind(store)}/>}/>
                     </Routes>
                 </div>
 
