714f0473c96ebedadb0cea54b050ef7afeec5dae
Alex Betke <arhontes@yandex.ru>
2022-06-17 15:41:58 +0300

    delete custom store subscriber (rerenderEntireTree)

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,10 @@ import store from "./Redux/redux-store";
 import {StateType} from "./Redux/store";
 import {Provider} from "react-redux";
 
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root')
+)
 
-
-
-const rerenderEntireTree = (state:StateType) => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App />
-        </Provider>
-         , document.getElementById('root')
-    )}
-
-store.subscribe(()=>rerenderEntireTree(store.getState()))
-rerenderEntireTree(store.getState())
