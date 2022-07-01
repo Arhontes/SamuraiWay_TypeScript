ef010838cfb926c2c432e6e0c554cc0f45f4c125
Alex Betke <arhontes@yandex.ru>
2022-07-01 17:10:20 +0300

    working with life cycle methods

--- src/Components/Users/UserClass.tsx
+++ src/Components/Users/UserClass.tsx
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 import {UsersPropsType} from "./UsersContainer";
-import {UserType} from "../../Redux/reducers/users-reducer";
 import User from "./User/User";
 import axios from "axios";
 
-class UserClass extends React.Component<UsersPropsType, {}> {
+class UserClass extends Component<UsersPropsType, {}> {
+    componentDidMount() {
+        alert("рендер успешно прошел, запускаю действие после вмонтирования")
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>this.props.setUsers(response.data.items))
+    }
+    componentDidUpdate(prevProps: Readonly<UsersPropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        alert("чувак это тот же самый объект, но пришли новые пропсы поэтому прошло обновление")
+    }
+    componentWillUpdate(nextProps: Readonly<UsersPropsType>, nextState: Readonly<{}>, nextContext: any) {
+        alert("ща обновлюсь")
+    }
+
+    componentWillUnmount() {
+        alert("черт меня удаляют но я вернусь!")
+    }
 
     render() {
+        alert("начал рендериться")
         return (
             <div>
-                <button onClick={()=>{
-                    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>this.props.setUsers(response.data.items))
-                }}>show users</button>
                 {this.props.users.map(el =>
                     <User id={el.id}
                           changeFollow={this.props.changeFollowed}
