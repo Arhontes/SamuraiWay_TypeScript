f2935583db39770a0a21268e66d9c4c1abe6a53b
Alex Betke <arhontes@yandex.ru>
2022-07-01 17:32:48 +0300

    network request in componentDidMount

--- src/Components/Users/UserClass.tsx
+++ src/Components/Users/UserClass.tsx
@@ -5,22 +5,10 @@ import axios from "axios";
 
 class UserClass extends Component<UsersPropsType, {}> {
     componentDidMount() {
-        alert("рендер успешно прошел, запускаю действие после вмонтирования")
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>this.props.setUsers(response.data.items))
-    }
-    componentDidUpdate(prevProps: Readonly<UsersPropsType>, prevState: Readonly<{}>, snapshot?: any) {
-        alert("чувак это тот же самый объект, но пришли новые пропсы поэтому прошло обновление")
-    }
-    componentWillUpdate(nextProps: Readonly<UsersPropsType>, nextState: Readonly<{}>, nextContext: any) {
-        alert("ща обновлюсь")
-    }
-
-    componentWillUnmount() {
-        alert("черт меня удаляют но я вернусь!")
     }
 
     render() {
-        alert("начал рендериться")
         return (
             <div>
                 {this.props.users.map(el =>
