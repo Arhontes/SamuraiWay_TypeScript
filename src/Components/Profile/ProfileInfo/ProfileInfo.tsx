7b074fe8d6df6a84fca426251877715471fdc33a
Alex Betke <arhontes@yandex.ru>
2022-07-03 19:59:39 +0300

    update ProfileInfo component for userProfile

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import p_i from './ProfileInfo.module.css'
-const ProfileInfo = () => {
+import style from './ProfileInfo.module.css'
+import {UserProfileType} from "../../../Redux/reducers/profile-page-reducer";
+
+type ProfileInfoPropsType = {
+    userProfile:UserProfileType
+}
+const ProfileInfo = (props:ProfileInfoPropsType) => {
     return (
-        <div className={p_i.profileInfo}>
-            <img src="https://res.cloudinary.com/teepublic/image/private/s--09E6bOfN--/c_crop,x_10,y_10/c_fit,h_1038/c_crop,g_north_west,h_1038,w_1038,x_-123,y_0/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-234,y_-111/b_rgb:0e121f/c_limit,f_jpg,h_630,q_90,w_630/v1606116559/production/designs/16396401_0.jpg" alt=""/>
-            <div className={p_i.p}> Text about me</div>
+        <div className={style.profileInfo}>
+            <img src={props.userProfile.photos?.small} alt=""/>
+            <div className={style.p}> {props.userProfile.aboutMe}</div>
         </div>
     );
 };
