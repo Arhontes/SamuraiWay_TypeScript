135022c5470b21209ce2787af3e0dade4c5de533
Alex Betke <arhontes@yandex.ru>
2022-07-03 20:00:45 +0300

    update Profile component

--- src/Components/Profile/Profile/Profile.tsx
+++ src/Components/Profile/Profile/Profile.tsx
@@ -4,19 +4,18 @@ import MyPosts from "../MyPosts/MyPosts";
 import ProfileInfo from "../ProfileInfo/ProfileInfo";
 import NewPostContainer from "../MyPosts/NewPost/NewPostContainer";
 import {ProfilePropsType} from "./ProfileContainer";
+import {Preloader} from "../../Common/Preloader/Preloader";
 
-const Profile = (props:ProfilePropsType) => {
+export const Profile = (props:ProfilePropsType) => {
     return (
         <div className={p.content}>
             <div className={p.wallpaper}>
                 <img src="https://wallpaperaccess.com/full/3725741.jpg" alt={""}/>
             </div>
 
-            <ProfileInfo />
+            {props.userProfile?<ProfileInfo userProfile={props.userProfile}/>:<Preloader/>}
             <NewPostContainer />
             <MyPosts posts={props.posts} />
         </div>
     );
 };
-
-export default Profile;
