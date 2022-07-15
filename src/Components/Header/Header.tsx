db217bf3182cb914ec89902538679c5c2017f449
Alex Betke <arhontes@yandex.ru>
2022-07-15 20:06:05 +0300

    create NavLink in Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import h from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <header className={h.header} >
             <img src="https://w7.pngwing.com/pngs/803/598/png-transparent-phoenix-logo-phoenix-red-bird-illustration-leaf-photography-mirror.png" alt=""/>
-
+            <div className={h.loginBlock}>
+                <NavLink to={"/login"}>Login</NavLink>
+            </div>
         </header>
     );
 };
