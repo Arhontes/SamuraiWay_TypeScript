cbb6957372e953cd0e903c51268f40433f75b69c
Alex Betke <arhontes@yandex.ru>
2022-07-16 11:40:35 +0300

    add HeaderPropsType

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import h from './Header.module.css'
 import {NavLink} from "react-router-dom";
-
-const Header = () => {
+type HeaderPropsType = {
+    isAuth:boolean
+}
+const Header = (props:HeaderPropsType) => {
     return (
         <header className={h.header} >
             <img src="https://w7.pngwing.com/pngs/803/598/png-transparent-phoenix-logo-phoenix-red-bird-illustration-leaf-photography-mirror.png" alt=""/>
             <div className={h.loginBlock}>
-                <NavLink to={"/login"}>Login</NavLink>
+                {props.isAuth?<NavLink to={"/logout"}>Logout</NavLink>:<NavLink to={"/login"}>Login</NavLink>}
             </div>
         </header>
     );
