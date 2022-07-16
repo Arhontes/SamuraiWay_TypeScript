fb88951e57a33712ef8496795e7c8200d3d8c484
Alex Betke <arhontes@yandex.ru>
2022-07-16 21:02:18 +0300

    aplly thunk middleware

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,17 +1,18 @@
-import {combineReducers, createStore, legacy_createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore, legacy_createStore} from "redux";
 import {profilePageReducer} from "./reducers/profile-page-reducer";
 import {dialogsPageReducer} from "./reducers/dialogs-page-reducer";
 import {usersReducer} from "./reducers/users-reducer";
 import {authReducer} from "./reducers/auth-reducer";
+import thunkMiddleware from "redux-thunk";
 
 export type  AppStateType = ReturnType<typeof rootReducer>
 
 const rootReducer = combineReducers({
-    profilePage:profilePageReducer,
-    dialogsPage:dialogsPageReducer,
-    users:usersReducer,
-    auth:authReducer,
+    profilePage: profilePageReducer,
+    dialogsPage: dialogsPageReducer,
+    users: usersReducer,
+    auth: authReducer,
 })
 
- let store = legacy_createStore(rootReducer)
+let store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export default store
