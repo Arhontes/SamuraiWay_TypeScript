cfe406387e724069367faca8e989223b53e975c6
Alex Betke <arhontes@yandex.ru>
2022-07-17 10:49:37 +0300

    refactoring

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -1,24 +1,17 @@
 import React, {Component, Dispatch} from 'react';
 import Header from "./Header";
-import axios from "axios";
 import {connect} from "react-redux";
 import {AppStateType} from "../../Redux/redux-store";
-import {setAuthUserDataAC} from "../../Redux/reducers/auth-reducer";
+import {authReducerThunkCreator, setAuthUserDataAC} from "../../Redux/reducers/auth-reducer";
 
 class HeaderClass extends Component<HeaderPropsType, {}> {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true}).then(response => {
-            if (response.data.resultCode === 0) {
-                this.props.setAuthUserData(response.data.data)
-            }
-        })
+        this.props.authMe()
     }
 
     render() {
         return (
-
                 <Header isAuth={this.props.isAuth}/>
-
         );
     }
 }
@@ -32,6 +25,8 @@ const mapStateToProps = (state: AppStateType): MapStatePropsType => {
 }
 type HeaderPropsType = MapStatePropsType&{
     setAuthUserData: typeof setAuthUserDataAC
+    authMe:()=>void
 }
 
-export const HeaderContainer = connect(mapStateToProps, {setAuthUserData: setAuthUserDataAC})(HeaderClass)
+export const HeaderContainer = connect(mapStateToProps, {setAuthUserData: setAuthUserDataAC,
+    authMe:authReducerThunkCreator})(HeaderClass)
--- src/Components/Profile/Profile/ProfileContainer.tsx
+++ src/Components/Profile/Profile/ProfileContainer.tsx
@@ -1,11 +1,12 @@
 import {PostType} from "../../../Redux/store";
 import {AppStateType} from "../../../Redux/redux-store";
 import {connect} from "react-redux";
-import {setUserProfileAC, UserProfileType} from "../../../Redux/reducers/profile-page-reducer";
+import {profilePageThunkCreator, setUserProfileAC, UserProfileType} from "../../../Redux/reducers/profile-page-reducer";
 import React, {useEffect} from "react";
 import axios from "axios";
 import {Profile} from "./Profile";
-import {useParams} from "react-router-dom";
+import {Params, useParams} from "react-router-dom";
+import {profilePageAPI} from "../../../api/api";
 
 
 type MapStatePropsType = {
@@ -13,7 +14,8 @@ type MapStatePropsType = {
     userProfile: UserProfileType | null
 }
 type MapDispatchPropsType = {
-    setUserProfile: (userProfile: UserProfileType) => void
+    getUserProfile: (params: Readonly<Params<string>>) => void
+
 }
 export type ProfilePropsType = MapStatePropsType & MapDispatchPropsType
 
@@ -22,19 +24,13 @@ let mapStateToProps = (state: AppStateType): MapStatePropsType => {
         posts: state.profilePage.posts,
         userProfile: state.profilePage.userProfile
     }
-
 }
 
 export function ProfileWrapper(props: ProfilePropsType) {
     let params = useParams();
-    console.log(params)
     if (params["*"]==='') params={"*":"2"}
     useEffect(() => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${params["*"]}`)
-            .then(response => {
-                props.setUserProfile(response.data)
-            })
-
+        props.getUserProfile(params)
     }, [])
 
     return (
@@ -43,4 +39,4 @@ export function ProfileWrapper(props: ProfilePropsType) {
 
 }
 
-export const ProfileContainer = connect(mapStateToProps, {setUserProfile: setUserProfileAC})(ProfileWrapper)
+export const ProfileContainer = connect(mapStateToProps, {getUserProfile: profilePageThunkCreator,})(ProfileWrapper)
